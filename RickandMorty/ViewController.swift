90afde499a279d5926727c998f23ca20b4d8ac43
andremietti <miettileandro@gmail.com>
2025-04-10 14:02:03 -0300

    Initial Commit

---
+++ RickandMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMorty
+//
+//  Created by andre mietti on 10/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
